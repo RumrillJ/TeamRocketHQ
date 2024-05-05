c75e81c62b1f6cc3b49324afe03a8c62ae1e5e88
rumrillj <john.rumrill@hotmail.com>
2024-05-04 21:39:43 -0700

    Updated NavBar

--- FrontEnd/p1-react-app/src/components/Navbar/Navbar.tsx
+++ FrontEnd/p1-react-app/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css'; 
 import profilePic from './teamrocketpfp.png'; 
+import settingsIcon from './settings.png'; 
 
 export const Navbar: React.FC = () => {
   const navigate = useNavigate();
@@ -15,19 +16,27 @@ export const Navbar: React.FC = () => {
     navigate('/home'); 
   };
 
+
+
   return (
     <nav className="navbar">
-      <div className="navbar-left"> 
-        <span className="navbar-title">Team Rocket HQ</span> 
+      <div className="navbar-left">
+        <span className="navbar-title">Team Rocket HQ</span>
       </div>
       <div className="navbar-links">
         <a href="/home" className="navbar-link">Home</a>
-        <a onClick={accessDenied} className="navbar-link">About</a> 
-        <a onClick={accessDenied} className="navbar-link">Contact</a> 
+        <a onClick={accessDenied} className="navbar-link">About</a>
+        <a onClick={accessDenied} className="navbar-link">Contact HR</a>
       </div>
-      <div className="navbar-signout"> 
-        <img src={profilePic} alt="Profile" className="navbar-profile" /> 
-        <button onClick={handleSignOut} className="signup-button">Sign Out</button> 
+      <div className="navbar-signout">
+        <img src={profilePic} alt="Profile" className="navbar-profile" />
+        <img
+          src={settingsIcon}
+          alt="Settings"
+          className="navbar-settings" 
+          onClick={accessDenied} 
+        />
+        <button onClick={handleSignOut} className="signup-button">Sign Out</button>
       </div>
     </nav>
   );
